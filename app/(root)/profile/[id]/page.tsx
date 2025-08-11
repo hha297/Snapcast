b5c68a945eb8e4fbae0732bcd56d7fbc635e0f36
hoangha297 <ha.dh290702@gmail.com>
2025-08-11 09:47:30 +0300

    User Profile

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,20 +1,49 @@
+import { EmptyState } from '@/components/EmptyState';
 import { Header } from '@/components/Header';
 import { VideoCard } from '@/components/VideoCard';
-import { dummyCards } from '@/constants';
+
+import { getAllVideosByUser } from '@/lib/actions/video';
+import { redirect } from 'next/navigation';
 import React from 'react';
 
-const ProfilePage = async ({ params }: ParamsWithSearch) => {
+const ProfilePage = async ({ params, searchParams }: ParamsWithSearch) => {
         const { id } = await params;
-        console.log(id);
+        const { query, filter } = await searchParams;
+
+        const { user, videos } = await getAllVideosByUser(id, query, filter);
+
+        if (!user) redirect('/sign-in');
 
         return (
                 <div className="wrapper page">
-                        <Header subHeader="ha.dh290702@gmail.com" title="Hoang Ha" userImg="/assets/images/dummy.jpg" />
-                        <section className="video-grid">
-                                {dummyCards.map((card) => (
-                                        <VideoCard key={card.id} {...card} />
-                                ))}
-                        </section>
+                        <Header
+                                subHeader={user.email}
+                                title={user.name}
+                                userImg={user.image || '/assets/images/dummy.jpg'}
+                        />
+
+                        {videos?.length > 0 ? (
+                                <>
+                                        <p>Your Videos</p>
+                                        <section className="video-grid">
+                                                {videos.map(({ videos, user }) => (
+                                                        <VideoCard
+                                                                key={videos?.id}
+                                                                {...videos}
+                                                                thumbnail={videos?.thumbnailUrl}
+                                                                userImg={user?.image || ''}
+                                                                username={user?.name || 'Guest'}
+                                                        />
+                                                ))}
+                                        </section>
+                                </>
+                        ) : (
+                                <EmptyState
+                                        icon="/assets/icons/video.svg"
+                                        title="No videos available"
+                                        description="Videos will appear here once you upload them"
+                                />
+                        )}
                 </div>
         );
 };
