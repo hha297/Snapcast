7879f1aa3e03e68fca15398a6b10c69fa0a9e414
hoangha297 <ha.dh290702@gmail.com>
2025-08-11 10:02:44 +0300

    Fix middleware bug in build

--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from 'next/server';
 import { auth } from '@/lib/auth';
 import { headers } from 'next/headers';
 import aj from './lib/arcjet';
-import { detectBot, shield } from '@arcjet/next';
+import { createMiddleware, detectBot, shield } from '@arcjet/next';
 
 export async function middleware(request: NextRequest, response: NextResponse) {
         const session = await auth.api.getSession({
@@ -20,7 +20,7 @@ const validate = aj
         .withRule(shield({ mode: 'LIVE' }))
         .withRule(detectBot({ mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE', 'GOOGLE_CRAWLER'] }));
 
-export default validate;
+export default createMiddleware(validate);
 
 export const config = {
         matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)'],
