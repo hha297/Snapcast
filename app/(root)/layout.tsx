397f19d83fbc2faa33a24e78dec255c407e21768
hoangha297 <ha.dh290702@gmail.com>
2025-08-11 10:14:44 +0300

    Fix middleware

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,7 +1,12 @@
 import { Navbar } from '@/components/Navbar';
+import { auth } from '@/lib/auth';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
 
-const Layout = ({ children }: { children: ReactNode }) => {
+const Layout = async ({ children }: { children: ReactNode }) => {
+        const session = await auth.api.getSession({ headers: await headers() });
+        if (!session) redirect('/sign-in');
         return (
                 <div>
                         <Navbar />
--- middleware.ts
+++ middleware.ts
@@ -1,28 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
-import { auth } from '@/lib/auth';
-import { headers } from 'next/headers';
 import aj from './lib/arcjet';
-import { shield, detectBot } from 'arcjet';
 import { createMiddleware } from '@arcjet/next';
-
-export async function middleware(request: NextRequest, response: NextResponse) {
-        const session = await auth.api.getSession({
-                headers: await headers(),
-        });
-
-        if (!session) {
-                return NextResponse.redirect(new URL('/sign-in', request.url));
-        }
-
-        return NextResponse.next();
-}
-
-const validate = aj
-        .withRule(shield({ mode: 'LIVE' }))
-        .withRule(detectBot({ mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE', 'GOOGLE_CRAWLER'] }));
-
-export default createMiddleware(validate);
+import { detectBot, shield } from 'arcjet';
 
 export const config = {
         matcher: ['/((?!api|_next/static|_next/image|favicon.ico|sign-in|assets).*)'],
 };
+
+export default createMiddleware(
+        aj
+                .withRule(shield({ mode: 'LIVE' }))
+                .withRule(detectBot({ mode: 'LIVE', allow: ['CATEGORY:SEARCH_ENGINE', 'GOOGLE_CRAWLER'] })),
+);
